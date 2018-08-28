273753f1266d93e6c03b352a0cde0eef1e47668c
alynielson <nielsonaly@gmail.com>
2018-08-28 09:42:50 -0500

    create Customer class and properties

---
+++ TrashCollectorProject/Models/Customer.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TrashCollectorProject.Models
+{
+    public class Customer
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string Address { get; set; }
+        public int ZipCode { get; set; }
+        
+        public DayOfWeek PickupDay { get; set; }
+        
+        public DateTime CustomStartDate { get; set; }
+        public DateTime CustomEndDate { get; set; }
+        public double MoneyOwed { get; set; }
+
+
+    }
+}
--- TrashCollectorProject/Startup.cs
+++ TrashCollectorProject/Startup.cs
@@ -10,5 +10,7 @@ namespace TrashCollectorProject
         {
             ConfigureAuth(app);
         }
+
+        
     }
 }
